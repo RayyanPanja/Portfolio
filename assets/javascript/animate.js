f453996a2cc5bbecd80e1f9824de9705973b7842
Rayyan Panja <113234549+RayyanPanja@users.noreply.github.com>
2022-11-25 15:36:04 +0530

    Dont Know??

--- assets/javascript/animate.js
+++ assets/javascript/animate.js
@@ -8,3 +8,20 @@ linkSet.forEach((link) => {
         link.appendChild(active);
     });
 });
+
+const SkillBtn = document.querySelector('#skilltitle');
+const Skills = document.querySelectorAll('.skill');
+Skills.forEach((skill) => { skill.style.opacity = 0 })
+
+SkillBtn.addEventListener('click', () => {
+    Skills.forEach((skill) => {
+        console.log("JJJ");
+        if (skill.style.opacity <= 0) {
+            skill.style.transition = "all 500ms";
+            skill.style.opacity += 1;
+        }else{
+            skill.style.opacity = 0;
+        }
+    })
+})
+
